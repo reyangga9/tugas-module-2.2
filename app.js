bc64242355e65723f833d9d51d713ac2a2fc4073
Anggara <reyangga5@gmail.com>
2023-07-17 14:34:56 +0000

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var songsRouter = require("./routes/songs");
 var app = express();
 
 app.use(express.json());
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/songs", songsRouter);
 
 app.listen(8080, function () {
   console.log("Listening on port 8080");
